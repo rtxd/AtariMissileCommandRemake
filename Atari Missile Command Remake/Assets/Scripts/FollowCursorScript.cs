1ba56c6c8b6cadfff8ef7005c0b8d01719f99634
Alex <alejandro.munoz@student.uts.edu.au>
2019-10-05 11:05:04 +1000

    Changed name to suit naming convention

--- Atari Missile Command Remake/Assets/Scripts/FollowCursorScript.cs
+++ Atari Missile Command Remake/Assets/Scripts/FollowCursorScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCursor : MonoBehaviour
+public class FollowCursorScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
