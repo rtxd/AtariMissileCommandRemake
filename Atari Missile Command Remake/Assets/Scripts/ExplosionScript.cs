34cb5af833e0a3da04871d87a45935412b017444
Alex <alejandro.munoz@student.uts.edu.au>
2019-10-10 16:27:40 +1100

    Explosions Setup

---
+++ Atari Missile Command Remake/Assets/Scripts/ExplosionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+    public Vector2 spawnPos;
+    public float explosionTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = spawnPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("I'm alive");
+        Destroy(gameObject, explosionTime);
+    }
+}
