e212eed673c2bd0d8860a87c8eb4154a71ede89a
Alex <alejandro.munoz@student.uts.edu.au>
2019-10-10 18:31:30 +1100

    Added Explosion functionality to enemy missiles

--- Atari Missile Command Remake/Assets/Scripts/EnemyMissileScript.cs
+++ Atari Missile Command Remake/Assets/Scripts/EnemyMissileScript.cs
@@ -8,8 +8,11 @@ public class EnemyMissileScript : MonoBehaviour
     Vector3 target;
     Vector3 startPosition;
     //Change this to dictate speed missile fires at
-    public float timeToReachTarget;
-
+    float timeToReachTarget;
+    public float explosionTime;
+    public Transform explosionPos;
+    public AudioClip explosionAudio;
+    public GameObject explosionAnim;
     float t;
 
     // Start is called before the first frame update
@@ -19,6 +22,7 @@ public class EnemyMissileScript : MonoBehaviour
         target = new Vector3(Random.Range(-5.0f, 5.0f), -6.0f, 0.0f);
         //Set the start position to the objects current position
         startPosition = transform.position;
+        timeToReachTarget = Random.Range(8, 16);
     }
 
     // Update is called once per frame
@@ -26,8 +30,6 @@ public class EnemyMissileScript : MonoBehaviour
     {
         move();
         rotate();
-
-        
     }
 
     //Move
@@ -45,6 +47,20 @@ public class EnemyMissileScript : MonoBehaviour
         Vector3 vectorToTarget = target - transform.position;
         float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, 1);
+    }
+
+    /// <summary>
+    /// Display animation for explosion and delete missile game object
+    /// </summary>
+    void explode()
+    {
+        AudioSource.PlayClipAtPoint(explosionAudio, new Vector3(0, 0, 0));
+        explosionAnim.GetComponent<ExplosionScript>().explosionTime = explosionTime;
+        explosionAnim.GetComponent<ExplosionScript>().spawnPos = transform.position;
+        Instantiate(explosionAnim, new Vector3(0, 0, 0), Quaternion.identity);
+
+
+        Destroy(this.gameObject);
     }
 }
